a40a4b3e377908bcacbb4f1782e735b6b0338c70
Mohammad Ghaemi <seyedmohammadghaem@gmail.com>
2018-07-13 10:58:50 -0400

    Fixed fingerprint matching

--- FingerMatcher.py
+++ FingerMatcher.py
@@ -8,7 +8,7 @@ Matches sample song's fingerprint and database's fingerprint
 Parameters
 ----------
 database: Dictionary with keys of (f1,f2,delta time) and values of [(song_name,t),...]
-sampleData: List of tuples (f1,f2, delta time)
+sampleData: List of tuples ((f1,f2, delta time), time of peak)
 
 Returns
 -------
@@ -18,11 +18,11 @@ String of song name
 
 def FingerprintMatcher(database, sampleData):
     idk = defaultdict(list) #
-    for index, item in enumerate(sampleData): #Loops through the sample audio and seperates the index and item (f1, f2, delta time)
+    for item, peakTime in sampleData: #Loops through the sample audio and seperates the index and item (f1, f2, delta time)
         for key in database: #Gets the key (f1, f2, delta time) 
             if key == item: #Checks if a key from database matches the item
                 for i in database.get(key):
-                    idk[(i[0],np.abs(index-i[1]))].append(1) 
+                    idk[(i[0],np.abs(peakTime-i[1]))].append(1) 
     return max(idk.items(), key=operator.itemgetter(1))[0][0] #Finds the song with the most timing similarities
 
     
