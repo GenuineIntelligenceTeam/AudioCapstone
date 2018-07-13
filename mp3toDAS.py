3688e8eb13fd923e9070319c444307988a5eb564
Ashley Teng <ashlaeteng@dhcp-18-111-1-32.dyn.MIT.EDU>
2018-07-13 09:35:45 -0400

    mp3 to DAS

---
+++ mp3toDAS.py
@@ -0,0 +1,30 @@
+from microphone import record_audio
+
+import numpy as np
+from audio_sampling import analog_to_digital, song_to_digital, turn_off_ticks
+import urllib
+import librosa
+import matplotlib.pyplot as plt
+from IPython.display import Audio
+from pathlib import Path
+
+
+def mp3ToDAS(song_list):
+    """ Parameters
+        ----------
+        song_list : array of songs
+
+
+        Returns
+        ----------
+        audio_data_mp3 : a numpy array that contains the digitized audio data"""
+    songs = [Path(".")/song_list]
+    local_song_path = songs[0]
+    samples, fs = librosa.load(local_song_path, sr=44100, mono=True, duration=11)
+
+    sampling_rate = 44100
+    quantizing_bits = 16
+    audio_data_mp3 = song_to_digital(local_song_path, sampling_rate, quantizing_bits)
+    return audio_data_mp3
+
+    # needs revision - revise based on where we will have mp3 files
