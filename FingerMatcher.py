54d20dc9447adcbe7de53f7c669e1098200d2625
Mohammad Ghaemi <seyedmohammadghaem@gmail.com>
2018-07-13 10:04:41 -0400

    Added comments

--- FingerMatcher.py
+++ FingerMatcher.py
@@ -2,14 +2,28 @@ import numpy as np
 from collections import defaultdict, Counter
 import operator
 
+'''
+Matches sample song's fingerprint and database's fingerprint
+
+Parameters
+----------
+database: Dictionary with keys of (f1,f2,delta time) and values of [(song_name,t),...]
+sampleData: List of tuples (f1,f2, delta time)
+
+Returns
+-------
+String of song name
+
+'''
+
 def FingerprintMatcher(database, sampleData):
-    idk = defaultdict(list)
-    for index, item in enumerate(sampleData):
-        for key in database:
-            if key == item:
+    idk = defaultdict(list) #
+    for index, item in enumerate(sampleData): #Loops through the sample audio and seperates the index and item (f1, f2, delta time)
+        for key in database: #Gets the key (f1, f2, delta time) 
+            if key == item: #Checks if a key from database matches the item
                 for i in database.get(key):
-                    idk[(i[0],np.abs(index-i[1]))].append(1)
-    return max(idk.items(), key=operator.itemgetter(1))[0][0]
+                    idk[(i[0],np.abs(index-i[1]))].append(1) 
+    return max(idk.items(), key=operator.itemgetter(1))[0][0] #Finds the song with the most timing similarities
 
     
 
